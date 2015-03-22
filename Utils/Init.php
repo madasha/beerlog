5eaf7b568c078704cfe28befa2563f988883640a
Mihail Irintchev <madasha@gmail.com>
2015-03-22 15:39:33 +0200

    Some fixed in menu items

--- Utils/Init.php
+++ Utils/Init.php
@@ -6,20 +6,18 @@ class Init
 	public static function initAll()
 	{
 		self::initBeerPostType();
+		self::initBreweryPostType();
 	}
 
 	public function addBeerMenus()
 	{
-		// TODO: Register autoloader and forget about this!
-		require_once BEERLOG_BASEDIR . '/Controllers/Admin.php';
-
 		$adminController = new \Beerlog\Controllers\Admin();
 		add_menu_page( 'Beer list', 'Biers', 'manage_options',
 			'edit.php?post_type=beerlog_beer', '',
 			BEERLOG_DIR_URL . 'assets/img/icons/beer.png', 8
 		);
-		// add_submenu_page( 'beerlog/beers_list.php', 'Breweries', 'Breweries', 'manage_options',
-			// 'beerlog/breweries.php', array( $adminController, 'renderBreweries' )
+		// add_submenu_page( 'edit.php?post_type=beerlog_beer', 'Breweries', 'Breweries',
+			// 'manage_options', 'edit.php?post_type=beerlog_brewery'
 		// );
 	}
 
@@ -28,7 +26,7 @@ class Init
 		$labels = array(
 			'name'               => __( 'Beers', 'beerlog' ),
 			'singular_name'      => __( 'Beer', 'beerlog' ),
-			'add_new'            => __( 'Add New', 'beerlog' ),
+			'add_new'            => __( 'Add New Beer', 'beerlog' ),
 			'add_new_item'       => __( 'Add New Beer', 'beerlog' ),
 			'edit_item'          => __( 'Edit Beer', 'beerlog' ),
 			'new_item'           => __( 'New Beer', 'beerlog' ),
@@ -54,5 +52,38 @@ class Init
 		);
 
 		register_post_type( 'beerlog_beer', $args );
+	}
+
+	public static function initBreweryPostType()
+	{
+		$labels = array(
+			'name'               => __( 'Breweries', 'beerlog' ),
+			'singular_name'      => __( 'Brewery', 'beerlog' ),
+			'add_new'            => __( 'Add New Brewery', 'beerlog' ),
+			'add_new_item'       => __( 'Add New Brewery', 'beerlog' ),
+			'edit_item'          => __( 'Edit Brewery', 'beerlog' ),
+			'new_item'           => __( 'New Brewery', 'beerlog' ),
+			'all_items'          => __( 'Breweries', 'beerlog' ),
+			'view_item'          => __( 'View Brewery', 'beerlog'),
+			'search_items'       => __( 'Search for breweries', 'beerlog' ),
+			'not_found'          => __( 'No breweries found', 'beerlog' ),
+			'not_found_in_trash' => __( 'No breweries found in the Trash', 'beerlog' ),
+			'menu_name'          => __( 'Breweries', 'beerlog' )
+		);
+
+		$args = array(
+			'labels'        	=> $labels,
+			'description'   	=> 'Holds breweries-related data',
+			'public'        	=> true,
+			'capability_type' 	=> 'post',
+			'hierarchical' 		=> false,
+			'taxonomies'		=> array('beerlog_style', 'beerlog_brewery'),
+			'has-archive' 		=> true,
+			'show_in_menu'  	=> 'edit.php?post_type=beerlog_beer',
+			'rewrite' 			=> array( 'slug' => 'breweries', 'with_front' => false, 'feeds' => true, 'pages' => true),
+			'supports'      	=> array( 'title', 'editor', 'thumbnail', 'revisions', 'comments'),
+		);
+
+		register_post_type( 'beerlog_brewery', $args );
 	}
 }
