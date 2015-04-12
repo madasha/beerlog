2c682929eb4aa401278095c493189421de32dab4
Mihail Irintchev <madasha@gmail.com>
2015-04-12 20:46:32 +0300

    Playing with the radar chart

--- templates/frontend/single-beerlog_beer.php
+++ templates/frontend/single-beerlog_beer.php
@@ -18,7 +18,10 @@ get_header(); ?>
     $mypost = array( 'post_type' => 'beerlog_beer' );
     $loop = new WP_Query( $mypost );
     ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
+    <?php while ( $loop->have_posts() ):
+        $post           = $loop->the_post();
+        $hasPropsChart  = get_post_meta( get_the_ID(), '_beerlog_meta_prop_chart', true );
+    ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header">
 
@@ -31,9 +34,9 @@ get_header(); ?>
                 <?php the_title(); ?><br />
 
                 <!-- Display yellow stars based on rating -->
-                <strong>Rating: </strong>
+                <strong><?php _e('Rating: ', 'beerlog'); ?></strong>
                 <?php
-                $nb_stars = intval( get_post_meta( get_the_ID(), 'movie_rating', true ) );
+                $nb_stars = intval( get_post_meta( get_the_ID(), 'beer_rating', true ) );
                 for ( $star_counter = 1; $star_counter <= 5; $star_counter++ ) {
                     if ( $star_counter <= $nb_stars ) {
                         echo '<img src="' . plugins_url( 'Movie-Reviews/images/icon.png' ) . '" />';
@@ -48,28 +51,64 @@ get_header(); ?>
             <div class="entry-content">
                 <?php the_content(); ?>
 
-                <strong>Director: </strong>
-                <?php echo esc_html( get_post_meta( get_the_ID(), 'movie_director', true ) ); ?>
+                <strong><?php _e('Alcohol (ABV): ', 'beerlog'); ?></strong>
+                <?php echo esc_html( get_post_meta( get_the_ID(), '_beerlog_meta_abv', true ) ); ?> %
                 <br />
 
-                <strong>Beer properties chart:</strong>
-                <div id="chart"></div>
+                <strong><?php _e('IBU: ', 'beerlog'); ?></strong>
+                <?php echo esc_html( get_post_meta( get_the_ID(), '_beerlog_meta_ibu', true ) ); ?>
+                <br />
+
+                <?php if ( true || $hasPropsChart ): ?>
+
+                    <strong><?php _e('Beer properties chart: ', 'beerlog'); ?></strong>
+                    <div id="chart"></div>
+
+                    <!-- Can use this chart to compare beers! -->
+
+                    <?php if ( true || 'pro' == $hasPropsChart ): ?>
+                    <script type="text/javascript">
+                    var beerData = [
+                        [
+                            {axis: "<?php _e('Fruty', 'beerlog'); ?>"       , value: 2},
+                            {axis: "<?php _e('Alcoholic', 'beerlog'); ?>"   , value: 4},
+                            {axis: "<?php _e('Citrus', 'beerlog'); ?>"      , value: 3},
+                            {axis: "<?php _e('Hoppy', 'beerlog'); ?>"       , value: 2},
+                            {axis: "<?php _e('Floral', 'beerlog'); ?>"      , value: 3},
+                            {axis: "<?php _e('Spicy', 'beerlog'); ?>"       , value: 5},
+                            {axis: "<?php _e('Malty', 'beerlog'); ?>"       , value: 4},
+                            {axis: "<?php _e('Toffee', 'beerlog'); ?>"      , value: 2},
+                            {axis: "<?php _e('Burnt', 'beerlog'); ?>"       , value: 4},
+                            {axis: "<?php _e('Sulphury', 'beerlog'); ?>"    , value: 1},
+                            {axis: "<?php _e('Sweet', 'beerlog'); ?>"       , value: 5},
+                            {axis: "<?php _e('Sour', 'beerlog'); ?>"        , value: 3},
+                            {axis: "<?php _e('Bitter', 'beerlog'); ?>"      , value: 4},
+                            {axis: "<?php _e('Dry', 'beerlog'); ?>"         , value: 2},
+                            {axis: "<?php _e('Body', 'beerlog'); ?>"        , value: 1},
+                            {axis: "<?php _e('Linger', 'beerlog'); ?>"      , value: 5},
+                        ]
+                    ];
+                    <?php else: ?>
+                    var beerData = [
+                        [
+                            {axis: "<?php _e('Sourness', 'beerlog'); ?>"    , value: 2},
+                            {axis: "<?php _e('Bitterness', 'beerlog'); ?>"  , value: 4},
+                            {axis: "<?php _e('Sweetness', 'beerlog'); ?>"   , value: 3},
+                            {axis: "<?php _e('Saltiness', 'beerlog'); ?>"   , value: 2},
+                            {axis: "<?php _e('Yeast', 'beerlog'); ?>"       , value: 3},
+                            {axis: "<?php _e('Hop', 'beerlog'); ?>"         , value: 5},
+                            {axis: "<?php _e('Malt', 'beerlog'); ?>"        , value: 4}
+                        ]
+                    ];
+                    <?php endif; ?>
+
+                    RadarChart.draw("#chart", beerData);
+                    </script>
+
+                <?php endif; ?>
             </div>
 
-            <!-- Can use this chart to compare beers! -->
-            <script type="text/javascript">
-            d = [
-                [
-                    {axis: "strength", value: 13},
-                    {axis: "intelligence", value: 1},
-                    {axis: "charisma", value: 8},
-                    {axis: "dexterity", value: 4},
-                    {axis: "luck", value: 9}
-                ]
-            ];
 
-            RadarChart.draw("#chart", d);
-            </script>
         </article>
 
     <?php endwhile; ?>
