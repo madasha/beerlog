e200ec0e5fc659a497d6151c213a0768e24269ca
Mihail Irintchev <madasha@gmail.com>
2015-04-29 00:14:37 +0300

    beer tests

--- tests/test-beerlog_beer.php
+++ tests/test-beerlog_beer.php
@@ -1,9 +1,13 @@
 <?php
-
 class BeerlogModelsBeerTest extends WP_UnitTestCase
 {
-	public function test_test()
+	public function test_beerPostCreate()
 	{
-		$this->assertTrue( true );
+		$beerId = $this->factory->post->create( array( 'post_type' => 'beerlog_beer' ) );
+		$this->assertTrue( is_int( $beerId ) && $beerId > 0 );
+
+		$beer = get_post( $beerId );
+		$this->assertTrue( $beer instanceof WP_Post );
+		$this->assertEquals( 'beerlog_beer', $beer->post_type );
 	}
 }
